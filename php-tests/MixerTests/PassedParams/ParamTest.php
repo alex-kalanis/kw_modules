28b4dde62efafb683479bf611ca1fc6e376b2678
Petr Plsek <me@kalanys.com>
2024-05-10 22:21:15 +0200

    kw_modules - decode plain entities in params

--- php-tests/MixerTests/PassedParams/ParamTest.php
+++ php-tests/MixerTests/PassedParams/ParamTest.php
@@ -15,6 +15,12 @@ class ParamTest extends CommonTestClass
         $this->assertEquals(['foo' => 'bar', 'abc' => 'def', 'ghi' => '12.5'], $lib->change('foo=bar&abc=def&ghi=12.5'));
     }
 
+    public function testQueryDeep(): void
+    {
+        $lib = new Mixer\PassedParams\HttpQuery();
+        $this->assertEquals(['foo' => 'bar', 'abc' => 'def', 'amp;ghi' => '12.5'], $lib->change('foo=bar&amp;abc=def&amp;amp;ghi=12.5'));
+    }
+
     public function testSingle(): void
     {
         $lib = new Mixer\PassedParams\SingleParam();
